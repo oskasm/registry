b6892f73e3f25af78a965fd42952d9019ace1699
Michael Kleger <mkleger@onesystems.ch>
2024-03-24 13:58:35 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'OneSystems GmbH Kasm Registry',
     description: 'The official store for Kasm supported workspaces.',
-    icon: 'https://www.onesystems.ch/wp-content/uploads/2018/01/logo-noframe.png',
+    icon: 'https://www.onesystems.ch/wp-content/uploads/2018/01/Favicon.png',
     listUrl: 'https://kasmregistry.onesystems.ch/',
     contactUrl: 'https://www.onesystems.ch/kontakt/',
   },
